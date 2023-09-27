d8d4ff1417bdc32a7d7267872e947120bdc1a576
etchepared <88214684+etchepared@users.noreply.github.com>
2023-09-27 19:13:44 -0300

    feat: add feat to filter products

--- my-fullstack-app/server/src/controllers/index.js
+++ my-fullstack-app/server/src/controllers/index.js
@@ -2,8 +2,20 @@ const products = require("../data/db.json");
 
 const getAllProducts = async (req, res) => {
   try {
-    if (products.length > 0) {
-      res.status(200).json(products);
+    const { category, price, condition } = req.query;
+
+    const filteredProducts = products.filter((product) => {
+      return (
+        (!category || product.category === category) &&
+        (!price || product.price <= price) &&
+        (!condition || product.condition === condition)
+      );
+    });
+
+    if (filteredProducts.length > 0) {
+      res.status(200).json(filteredProducts);
+    } else {
+      res.status(404).json({ message: "No products found" });
     }
   } catch (error) {
     res.status(500).json({ message: error.message });
